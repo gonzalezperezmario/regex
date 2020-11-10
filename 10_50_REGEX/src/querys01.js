212c7733a8d77417be0dcabfbf5292a48f18289f
gonzalezperezmario <73132372+gonzalezperezmario@users.noreply.github.com>
2020-11-10 20:05:23 +0100

    Delete querys01.js

--- 10_50_REGEX/src/querys01.js
+++
@@ -1,7 +0,0 @@
-
-
-db . graficas . find ({ $nor: [{  precio: {  $in: ["150","300"] }}, { tdp:{  $gte: 90} } ] } )
-
-db . graficas . find ({ $or: [{  ensambladora: {  $eq: "Nvidia" }}, { fecha:{  $lt: ("2017, 5, 17")} } ] } )
-
-
